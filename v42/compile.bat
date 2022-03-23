0e3c7e7cef77da6087edee80b5e1bc966310c412
arctocore <steenosterby@arctocore.eu>
2022-03-23 10:41:06 +0100

    new version 4.2
    new version 4.2

---
+++ v42/compile.bat
@@ -0,0 +1,26 @@
+cd C:\Users\arcto\Documents\GitHub\mobileappfree\v42
+java -jar compiler.jar --js c2runtime.js --js_output_file c2runtime1.js
+java -jar compiler.jar --js sporttactic.js --js_output_file sporttactic1.js
+java -jar compiler.jar --js sw.js --js_output_file sw1.js
+java -jar compiler.jar --js adapter.js --js_output_file adapter1.js
+
+del c2runtime.js
+del sporttactic.js
+del sw.js
+del adapter.js
+
+ren c2runtime1.js c2runtime.js
+ren sporttactic1.js sporttactic.js
+ren sw1.js sw.js
+ren adapter1.js adapter.js
+
+ 
+
+
+
+EXIT
+
+
+
+
+
